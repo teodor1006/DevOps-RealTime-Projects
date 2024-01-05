149f733722052b62ad2cc1eea227810b29a8f3b3
teodor1006 <tedi_montana@abv.bg>
2024-01-05 15:18:17 +0200

    s3 bucket policy + s3 object

---
+++ StaticWebsiteHostingAmazonS3/aws_s3_bucket_policy.tf
@@ -0,0 +1,29 @@
+resource "aws_s3_bucket_policy" "allow_cloudfront" {
+  bucket = aws_s3_bucket.static_website.id
+  policy = data.aws_iam_policy_document.cloudfront.json
+}
+
+data "aws_iam_policy_document" "cloudfront" {
+  statement {
+    sid     = "AllowCloudFrontServicePrincipalReadOnly"
+    effect  = "Allow"
+    actions = ["s3:GetObject"]
+    resources = [
+      aws_s3_bucket.static_website.arn,
+      "${aws_s3_bucket.static_website.arn}/*"
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values = [
+        aws_cloudfront_distribution.s3_distribution.arn
+      ]
+    }
+  }
+}
