be940a6f72169586e141644ae9a5ff5eff38a8d5
teodor1006 <tedi_montana@abv.bg>
2024-01-05 13:56:53 +0200

    website files + variables.tf

---
+++ StaticWebsiteHostingAmazonS3/variables.tf
@@ -0,0 +1,20 @@
+variable "aws_region" {
+    type = string
+}
+
+variable "prefix" {
+  type = string
+}
+
+variable "domain_name" {
+  type = string
+}
+
+variable "bucket_name" {
+  type = string
+}
+
+variable "common_tags" {
+  type = map(string)
+  description = "Common tags you want applied to all components"
+}
