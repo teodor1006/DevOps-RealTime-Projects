7bb3ae5f379ae6d5ee23a73706eb435f7f915ac4
teodor1006 <tedi_montana@abv.bg>
2024-01-05 14:20:34 +0200

    create an s3 bucket

---
+++ StaticWebsiteHostingAmazonS3/aws_s3_bucket.tf
@@ -0,0 +1,43 @@
+resource "aws_s3_bucket" "static_website" {
+    bucket = var.bucket_name
+
+    tags = var.common_tags
+}
+
+resource "aws_s3_bucket_website_configuration" "website_bucket" {
+    bucket = aws_s3_bucket.static_website.id
+    
+    index_document {
+      suffix = "index.html"
+    }
+
+    error_document {
+      key = "index.html"
+    }
+}
+
+resource "aws_s3_bucket_ownership_controls" "website_bucket" {
+  bucket = aws_s3_bucket.static_website.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "website_bucket" {
+  bucket = aws_s3_bucket.static_website.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_acl" "website_bucket" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.website_bucket,
+    aws_s3_bucket_public_access_block.website_bucket,
+  ]
+
+  bucket = aws_s3_bucket.static_website.id
+  acl    = "private"
+}
