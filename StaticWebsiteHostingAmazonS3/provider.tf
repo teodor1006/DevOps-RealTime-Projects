6afee1b4eac55b5efc21539f8436e5a872d1e7ac
teodor1006 <tedi_montana@abv.bg>
2024-01-05 14:03:57 +0200

    local vars + provider

---
+++ StaticWebsiteHostingAmazonS3/provider.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+    region = var.aws_region
+}
+
+#Cloudfront requires SSL Certificates to be provisioned in N.Virginia (us-east-1) region
+provider "aws" {
+    alias = "acm_provider"
+    region = "us-east-1"
+}
